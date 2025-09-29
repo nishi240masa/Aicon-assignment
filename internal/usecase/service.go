9aa85d544595575fee889bf987b25f5922bf8846
nishi240masa <nishi240masa@gmail.com>
2025-09-29 15:58:07 +0900

    feat: Serviceレイヤーに更新用の入力構造体を追加

--- internal/usecase/service.go
+++ internal/usecase/service.go
@@ -24,9 +24,15 @@ type CreateItemInput struct {
 	PurchaseDate  string `json:"purchase_date"`
 }
 
+type UpdateItemInput struct {
+	Name          *string `json:"name,omitempty"`
+	Brand         *string `json:"brand,omitempty"`
+	PurchasePrice *int    `json:"purchase_price,omitempty"`
+}
+
 type CategorySummary struct {
 	Categories map[string]int `json:"categories"`
-	Total      int            `json:"total"`
+	Total      int           `json:"total"`
 }
 
 type itemUsecase struct {
